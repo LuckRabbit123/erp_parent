47ec9e69d4c6e4489795b205223047069e62a7a2
yufan <18673517123@163.com>
2019-01-08 14:25:42 +0800

    test

--- erp_biz/src/main/java/cn/itcast/biz/impl/DepBiz.java
+++ erp_biz/src/main/java/cn/itcast/biz/impl/DepBiz.java
@@ -6,37 +6,12 @@ import cn.itcast.erp.entity.Dep;
 
 import java.util.List;
 
-public class DepBiz implements IDepBiz {
+public class DepBiz extends BaseBiz<Dep> implements IDepBiz {
     private IDepDao depDao;
 
     public void setDepDao(IDepDao depDao) {
         this.depDao = depDao;
+        super.setBaseDao(this.depDao);
     }
 
-    public List<Dep> getList() {
-        return depDao.getList();
-    }
-
-    public List<Dep> getList(Dep dep1, Dep dep2, Object param,int firstResult,int maxResuts) {
-        return depDao.getList(dep1,dep2, param,firstResult,maxResuts);
-    }
-    public Long getCount(Dep dep1, Dep dep2, Object param){
-        return depDao.getCount(dep1,dep2,param);
-    }
-
-    public void add(Dep dep) {
-       depDao.add(dep);
-    }
-
-    public void delete(Long uuid) {
-        depDao.delete(uuid);
-    }
-
-    public Dep get(Long uuid) {
-        return depDao.get(uuid);
-    }
-
-    public void update(Dep dep) {
-        depDao.update(dep);
-    }
 }
