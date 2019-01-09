83d131a20d7a2a6d2824f9846f298ef7df2a1b6b
yufan <18673517123@163.com>
2019-01-09 15:24:54 +0800

    test1

--- erp_dao/src/main/java/cn/itcast/dao/impl/EmpDao.java
+++ erp_dao/src/main/java/cn/itcast/dao/impl/EmpDao.java
@@ -1,7 +1,6 @@
 package cn.itcast.dao.impl;
 
 import cn.itcast.dao.IEmpDao;
-import cn.itcast.erp.entity.Dep;
 import cn.itcast.erp.entity.Emp;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.MatchMode;
@@ -10,7 +9,7 @@ import org.hibernate.criterion.Restrictions;
 public class EmpDao extends BaseDao<Emp> implements IEmpDao {
 
     public DetachedCriteria getDetachedCriteria(Emp emp1, Emp emp2, Object param){
-        DetachedCriteria dc=DetachedCriteria.forClass(Dep.class);
+        DetachedCriteria dc=DetachedCriteria.forClass(Emp.class);
         if(emp1!=null){
             if(null != emp1.getName() && emp1.getName().trim().length()>0){
                 dc.add(Restrictions.like("name", emp1.getName(), MatchMode.ANYWHERE));
