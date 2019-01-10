c744021cfcdf462ea51b38b7f0be59c571d53985
yufan <18673517123@163.com>
2019-01-10 09:43:18 +0800

    test1

--- erp_dao/src/main/java/cn/itcast/dao/impl/EmpDao.java
+++ erp_dao/src/main/java/cn/itcast/dao/impl/EmpDao.java
@@ -9,13 +9,33 @@ import org.hibernate.criterion.Restrictions;
 public class EmpDao extends BaseDao<Emp> implements IEmpDao {
 
     public DetachedCriteria getDetachedCriteria(Emp emp1, Emp emp2, Object param){
-        DetachedCriteria dc=DetachedCriteria.forClass(Emp.class);
-        if(emp1!=null){
-            if(null != emp1.getName() && emp1.getName().trim().length()>0){
+        DetachedCriteria dc = DetachedCriteria.forClass(Emp.class);
+        if (emp1 != null) {
+            if (null != emp1.getUsername() && emp1.getUsername().trim().length() > 0) {
+                dc.add(Restrictions.like("username", emp1.getUsername(), MatchMode.ANYWHERE));
+            }
+            if (null != emp1.getPwd() && emp1.getPwd().trim().length() > 0) {
+                dc.add(Restrictions.like("pwd", emp1.getPwd(), MatchMode.ANYWHERE));
+            }
+            if (null != emp1.getName() && emp1.getName().trim().length() > 0) {
                 dc.add(Restrictions.like("name", emp1.getName(), MatchMode.ANYWHERE));
             }
-            if(null != emp1.getTele() && emp1.getTele().trim().length()>0){
+            if (null != emp1.getEmail() && emp1.getEmail().trim().length() > 0) {
+                dc.add(Restrictions.like("email", emp1.getEmail(), MatchMode.ANYWHERE));
+            }
+            if (null != emp1.getTele() && emp1.getTele().trim().length() > 0) {
                 dc.add(Restrictions.like("tele", emp1.getTele(), MatchMode.ANYWHERE));
+            }
+            if (null != emp1.getAddress() && emp1.getAddress().trim().length() > 0) {
+                dc.add(Restrictions.like("address", emp1.getAddress(), MatchMode.ANYWHERE));
+            }
+            //根据部门查询
+            if (null != emp1.getDep() && null != emp1.getDep().getUuid()) {
+                dc.add(Restrictions.eq("dep", emp1.getDep()));
+            }
+            //出生年月日查询 起始日期
+            if (null != emp1.getBirthday()) {
+                dc.add(Restrictions.ge("birthday", emp1.getBirthday()));
             }
 
         }
